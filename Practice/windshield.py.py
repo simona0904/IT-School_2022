a5a852cd75e541d7337f8fdcec5a057ca239e152
Simona Cordos <cordos.simona@gmail.com>
2022-05-23 10:20:57 +0300

    add IT-School_2022

---
+++ Practice/windshield.py.py
@@ -0,0 +1,40 @@
+
+# def monthly_salary(name, salary):
+#     print("Monthly salary for " + name + " is " + str(salary))
+
+# monthly_salary('Simona', 1000)    
+# monthly_salary('Bogdan', 1200)
+# monthly_salary('Alina', 1500)    
+# monthly_salary('Ciprian', 1000)    
+
+
+# def windshield_prices(producer, car_type, price):
+#     print('Price for ' + producer + ' ' + car_type + ' is ' + str(price) + ' ron.')
+
+# windshield_prices('Opel', 'Corsa c', 330)    
+# windshield_prices('Mazda', '626', 450)
+# windshield_prices('Volkswagen', 'Golf 5', 480)
+# windshield_prices('Fiat', 'Punto ', 380)
+# windshield_prices('Audi', 'A 4', 550)            
+    
+        
+# Functie care primeste o lista de nr si returneaza cel mai mic:
+ 
+def is_the_lowest_nr(number_list):
+    number_list.sort() 
+
+    return number_list[0]
+        
+        
+is_the_lowest_nr([100, 3, 45]) 
+print(is_the_lowest_nr([100, 3, 45]))
+
+
+
+
+     
+
+    
+
+    
+
