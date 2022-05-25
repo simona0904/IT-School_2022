5860c64c9befc6ebbc56e75cd0a0a8fedefbb907
Simona Cordos <cordos.simona@gmail.com>
2022-05-25 17:25:44 +0300

    add GIT files

---
+++ S7.GIT/S7 GIT.py
@@ -0,0 +1,63 @@
+# Version Control Systems
+
+# Protocolul GIT
+
+# GIT server -> PC 1 -> REPO
+#            -> PC 2 -> REPO (in repo se salveaza structuri de date numite COMITURI)
+
+# Implementari cunoscute:
+
+# GITHUB
+# GITLAB
+# BITBUCKET
+
+
+# GIT CLI
+
+# Comenzi utile in GIT:
+
+# Initializare repo:
+# git init
+
+# clone repo = clonare cod:
+# git clone URL
+
+# Verificare stare:
+# git status 
+
+# Adaugare fisiere pt a fi incluse in urm commit:
+# git add NUME FISIER
+
+# Adaugare toate fisierele:
+# git add
+
+# Stergere fisiere din lista de modificari pt commit:
+# git rm --cached NUME FISIER
+
+# commit = salveaza starea curenta
+# git commit -m "Mesaj"
+# git commit
+# se iese din editor cu ESC > : > wq > enter
+
+# afiseaza configuratia:
+# git config --global -l
+
+# configureaza email si nume:
+# git config --global user.email "EMAIL GITHUB"
+# git config --global user.name "Nume prenume"
+
+# afiseaza istorie:
+# git log
+
+# intoarcere in timp - reset la commit:
+# git reset COMMIT_ID
+
+# Uploadam modificari: git push
+# Descarcam cele mai noi modificari: git pull
+
+
+
+
+
+
+
