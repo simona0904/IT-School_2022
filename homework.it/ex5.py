0282fe325a1ace8bdfd75487422c704e1c26bbc0
Simona Cordos <cordos.simona@gmail.com>
2022-06-20 18:06:39 +0300

    add IT School

--- homework.it/ex5.py
+++
@@ -1,8 +0,0 @@
-
-# Extract the number from import_numbers variable.
-
-import_numbers = "Today we had 280 clients."
-
-only_numbers = [int(i) for i in import_numbers.split() if i.isdigit()]
-print(only_numbers)
-
