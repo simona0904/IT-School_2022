f3b9d8b88177fc301a5201baf6627d5e07c53b6c
Simona Cordos <cordos.simona@gmail.com>
2022-06-20 17:56:44 +0300

    add IT School

--- homework.it/hw.datetime/ex1.py
+++ homework.it/hw.datetime/ex1.py
@@ -7,9 +7,9 @@ def print_msg_based_on_time():
     current_hour = int(datetime.now().strftime("%H"))
     if current_hour <= 12:
         print("Good morning")
-    elif current_hour > 12 and current_hour <= 17:
+    elif current_hour > 12 and current_hour <= 18:
         print("Good afternoon")  
-    elif current_hour > 17 and current_hour <= 21:
+    elif current_hour > 18 and current_hour <= 21:
         print("Good evening.")  
     else:
         print("Good night")     
